053b0a2aefd7841a3bb80ecb4eb1b2524dfa81a1
William John Holden <wjholden@gmail.com>
2025-12-06 18:56:53 +0100

    Update day06.rs

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,3 +1,5 @@
+use std::panic;
+
 use advent_of_code_2025::*;
 use nalgebra::DMatrix;
 
@@ -9,6 +11,10 @@ const PUZZLE: &str = include_str!("../../puzzles/day06.txt");
 /// Sometimes you're proud of your fast and elegant AoC solution. Other times
 /// you try not to think about the index-array horrors of your ugly code.
 /// This is one of those latter times.
+///
+/// Later comment: so I implemented a very clever idea to use matrix
+/// multiplication (thank you Reddit), but by my benchmarks it isn't reliably
+/// faster. Oh well.
 fn main() {
     let d = Puzzle::new(PUZZLE);
     let d = d.solve();
@@ -22,8 +28,128 @@ pub struct Puzzle {
     pub part1: usize,
     pub part2: usize,
     matrix: DMatrix<usize>,
+    #[cfg(not(feature = "faster"))]
     vertical_numbers: Vec<Vec<usize>>,
+    #[cfg(feature = "faster")]
+    z: DMatrix<usize>,
     operators: Vec<String>,
+}
+
+#[cfg(not(feature = "faster"))]
+fn parse_part2(input: &str, rows: usize, cols: usize) -> Vec<Vec<usize>> {
+    // Vertical parsing. I suddenly sympathize for those working with eastern Asian languages.
+    let mut start_markers: Vec<usize> = input
+        .lines()
+        .last()
+        .unwrap()
+        .char_indices()
+        .filter_map(|(i, c)| match c {
+            '+' | '*' => Some(i),
+            ' ' => None,
+            _ => unreachable!(),
+        })
+        .collect();
+    start_markers.push(input.lines().next().unwrap().len() + 1); // for the last token.
+    let widths: Vec<(usize, usize)> = start_markers
+        .windows(2)
+        .filter_map(|w| {
+            if let &[start, end] = w {
+                Some((start, end - 1))
+            } else {
+                None
+            }
+        })
+        .collect();
+    assert_eq!(widths.len(), cols);
+    let lines: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
+
+    let mut vertical_numbers = Vec::new();
+    for (start, end) in widths {
+        let group_width = end - start;
+        let mut vertical_group = vec![0; group_width];
+        for c in start..end {
+            for r in 0..rows {
+                // don't use .chars().nth(), it is really slow.
+                match lines[r][c] as char {
+                    ' ' => {}
+                    d @ '1'..='9' => {
+                        vertical_group[c - start] =
+                            10 * vertical_group[c - start] + d as usize - '0' as usize;
+                    }
+                    other => {
+                        eprintln!("unexpected {other}");
+                        panic!();
+                    }
+                }
+            }
+        }
+        vertical_numbers.push(vertical_group);
+    }
+    vertical_numbers
+}
+
+#[cfg(feature = "faster")]
+fn parse_part2(input: &str, rows: usize) -> DMatrix<usize> {
+    let cols_wide = input.lines().next().unwrap().len();
+    let mut y = DMatrix::<usize>::zeros(rows, cols_wide);
+    for (row, line) in input.lines().take(rows).enumerate() {
+        for (col, c) in line.char_indices() {
+            y[(row, col)] = match c {
+                ' ' => 0,
+                d @ '1'..='9' => d as usize - '0' as usize,
+                x => {
+                    eprintln!("unexpected symbol {x}");
+                    panic!();
+                }
+            }
+        }
+    }
+    // Yeah, I know. It's a bubble sort variant. Whatever, nrows<10.
+    for col in 0..y.ncols() {
+        for r1 in (0..y.nrows() - 1).rev() {
+            for r2 in (1..y.nrows()).rev() {
+                if y[(r2, col)] == 0 {
+                    y.swap((r1, col), (r2, col));
+                }
+            }
+        }
+    }
+    let x = DMatrix::from_iterator(
+        1,
+        rows,
+        (0..rows).map(|i| 10usize.pow(rows as u32 - i as u32 - 1)),
+    );
+    let z = x * y;
+    z
+}
+
+impl Puzzle {
+    #[cfg(not(feature = "faster"))]
+    fn part2(mut self) -> Self {
+        for (operator, list) in self.operators.iter().zip(self.vertical_numbers.iter()) {
+            let x = match operator.as_str() {
+                "+" => list.iter().sum::<usize>(),
+                "*" => list.iter().product::<usize>(),
+                _ => panic!("unexpected operator"),
+            };
+            self.part2 += x;
+        }
+        self
+    }
+
+    #[cfg(feature = "faster")]
+    fn part2(mut self) -> Self {
+        let mut it = self.z.iter();
+        for operator in self.operators.iter() {
+            let x = match operator.as_str() {
+                "+" => it.by_ref().take_while(|x| **x > 0).sum::<usize>(),
+                "*" => it.by_ref().take_while(|x| **x > 0).product::<usize>(),
+                _ => panic!("unexpected operator"),
+            };
+            self.part2 += x;
+        }
+        self
+    }
 }
 
 impl Solver for Puzzle {
@@ -37,71 +163,25 @@ impl Solver for Puzzle {
             .count();
         let mut operators = Vec::new();
 
-        let mut matrix = DMatrix::zeros(rows, cols);
+        let mut m1 = DMatrix::zeros(rows, cols);
 
         for (row, line) in input.lines().enumerate() {
             for (col, word) in line.split_whitespace().enumerate() {
                 match word.parse::<usize>() {
-                    Ok(x) => matrix[(row, col)] = x,
+                    Ok(x) => m1[(row, col)] = x,
                     Err(_) => operators.push(word.to_owned()),
                 }
             }
         }
 
-        // Vertical parsing. I suddenly sympathize for those working with eastern Asian languages.
-        let mut start_markers: Vec<usize> = input
-            .lines()
-            .last()
-            .unwrap()
-            .char_indices()
-            .filter_map(|(i, c)| match c {
-                '+' | '*' => Some(i),
-                ' ' => None,
-                _ => unreachable!(),
-            })
-            .collect();
-        start_markers.push(input.lines().next().unwrap().len() + 1); // for the last token.
-        let widths: Vec<(usize, usize)> = start_markers
-            .windows(2)
-            .filter_map(|w| {
-                if let &[start, end] = w {
-                    Some((start, end - 1))
-                } else {
-                    None
-                }
-            })
-            .collect();
-        assert_eq!(widths.len(), cols);
-        let lines: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
-
-        let mut vertical_numbers = Vec::new();
-        for (start, end) in widths {
-            let group_width = end - start;
-            let mut vertical_group = vec![0; group_width];
-            for c in start..end {
-                for r in 0..rows {
-                    // don't use .chars().nth(), it is really slow.
-                    match lines[r][c] as char {
-                        ' ' => {}
-                        d @ '1'..='9' => {
-                            vertical_group[c - start] =
-                                10 * vertical_group[c - start] + d as usize - '0' as usize;
-                        }
-                        other => {
-                            eprintln!("unexpected {other}");
-                            panic!();
-                        }
-                    }
-                }
-            }
-            vertical_numbers.push(vertical_group);
-        }
-
         Self {
             part1: 0,
             part2: 0,
-            matrix,
-            vertical_numbers,
+            matrix: m1,
+            #[cfg(not(feature = "faster"))]
+            vertical_numbers: parse_part2(input, rows, cols),
+            #[cfg(feature = "faster")]
+            z: parse_part2(input, rows),
             operators,
         }
     }
@@ -116,14 +196,7 @@ impl Solver for Puzzle {
             self.part1 += x;
         }
 
-        for (operator, list) in self.operators.iter().zip(self.vertical_numbers.iter()) {
-            let x = match operator.as_str() {
-                "+" => list.iter().sum::<usize>(),
-                "*" => list.iter().product::<usize>(),
-                _ => panic!("unexpected operator"),
-            };
-            self.part2 += x;
-        }
+        self = self.part2();
 
         self
     }
